6bc473fcae13cdf1f5e10312e3a52eae3ae119f5
Fradm98 <fradm@kth.se>
2023-11-10 13:22:09 +0000

    utils

--- src/qupytex/utils.py
+++ src/qupytex/utils.py
@@ -18,3 +18,41 @@ def create_sequential_colors(num_colors, colormap_name):
     colormap_values = np.linspace(0, 1, num_colors)
     colors = [colormap(value) for value in colormap_values]
     return colors
+
+# ---------------------------------------------------------------------------------------
+# Open rdms
+# ---------------------------------------------------------------------------------------
+def open_rdms(file_path: str):
+    """
+    open_rdms
+
+    This function loads the saved rmds (1 qubit rdm 2x2) as
+    numpy arrays.
+
+    file_path: str - path and name of the file to open
+    
+    """
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    X = []
+    for line in lines:
+        line1 = line.split(" ")
+        op = '['
+        cl = ']'
+        line2 = []
+        for elem in line1:
+            if len(elem) > 1 and '\n' not in elem:
+                if op in elem:
+                    elem = elem.replace(op,'')
+                elif cl in elem:  
+                    elem = elem.replace(cl,'')
+
+                line2.append(float(elem))
+            elif len(elem) > 3 and ']\n' in elem:  
+                elem = elem.replace(']\n','')
+                line2.append(float(elem))
+                    
+        rdm = np.array(line2).reshape(2,2)
+        X.append(rdm)
+    return X
